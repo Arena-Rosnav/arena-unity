23edd95eafa78b2741ee75bad65f7c3a93373eda
TheZomb <osmaevhalid@outlook.com>
2023-11-07 00:35:31 +0100

    example handler for spawn_model

--- Assets/ServiceController.cs
+++ Assets/ServiceController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 using Unity.Robotics.ROSTCPConnector.ROSGeometry;
 
-using RosMessageTypes.Ford;
+using RosMessageTypes.Std;
 
 /// <summary>
 /// Example demonstration of implementing a UnityService that receives a Request message from another ROS node and sends a Response back
@@ -10,27 +10,50 @@ using RosMessageTypes.Ford;
 public class ServiceController : MonoBehaviour
 {
     [SerializeField]
-    string m_ServiceName = "unity/spawn_model";
+    string SpawnServiceName = "unity/spawn_model";
+    [SerializeField]
+    string DeleteServiceName = "unity/delete_model";
+    [SerializeField]
+    string MoveServiceName = "unity/set_model_state";
+    [SerializeField]
+    string GoalServiceName = "unity/set_goal";
 
     void Start()
     {
-        // register the service with ROS
-        ROSConnection.GetOrCreateInstance().ImplementService<GetSafeActionsRequest, GetSafeActionsResponse>(m_ServiceName, GetObjectPose);
+        // register the services with ROS
+        ROSConnection.GetOrCreateInstance().ImplementService<EmptyRequest, EmptyResponse>(SpawnServiceName, HandleSpawn);
+        ROSConnection.GetOrCreateInstance().ImplementService<EmptyRequest, EmptyResponse>(DeleteServiceName, Example);
+        ROSConnection.GetOrCreateInstance().ImplementService<EmptyRequest, EmptyResponse>(MoveServiceName, Example);
+        ROSConnection.GetOrCreateInstance().ImplementService<EmptyRequest, EmptyResponse>(GoalServiceName, Example);
     }
+
+    /// HANDLER SECTION
+    /// Put here all the functions on how top handle the service requests
 
     /// <summary>
     ///  Callback to respond to the request
     /// </summary>
     /// <param name="request">service request containing the object name</param>
     /// <returns>service response containing the object pose (or 0 if object not found)</returns>
-    private GetSafeActionsResponse GetObjectPose(GetSafeActionsRequest request)
+    private EmptyResponse Example(EmptyRequest request)
     {
         // process the service request
         Debug.Log("Received request for object: " + request);
 
         // prepare a response
-        GetSafeActionsResponse objectPoseResponse = new GetSafeActionsResponse();
+        EmptyResponse objectPoseResponse = new EmptyResponse();
 
         return objectPoseResponse;
+    }
+
+    private EmptyResponse HandleSpawn(EmptyRequest request)
+    {
+        // process the service request
+        Debug.Log("SPAWN REQUEST of:" + request.ToString());
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(Random.Range(-10.0f, 10.0f), 5, Random.Range(-10.0f, 10.0f));
+
+        return new EmptyResponse();
     }
 }
