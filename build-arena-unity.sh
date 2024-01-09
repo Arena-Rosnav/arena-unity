ef782a6d3a7db321a409360280778a82d6f80ac0
MaximilianSchreff <schreffmax@gmail.com>
2024-01-09 11:26:17 +0100

    Shell script for building unity

---
+++ build-arena-unity.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -i
+
+unity_location="${HOME}/Unity/Hub/Editor/2022.3.11f1/Editor/Unity"
+
+project_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+build_path="${project_path}/build/arena-unity.x86_64"
+
+build_log="${project_path}/build/build_log.txt"
+
+ARENA_SIM_SETUP="$(cd "$project_path"/../arena-simulation-setup && pwd)"
+
+# Export arena_simulation_setup location for arena-unity to find files
+export ARENA_SIM_SETUP
+
+# Build Unity
+"$unity_location" -quit -batchmode -projectpath "$project_path" -buildLinux64Player "$build_path" -logFile "$build_log"
