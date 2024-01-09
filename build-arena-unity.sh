922a1f2a70f8a0a6192aeae4bbb334737a21553e
MaximilianSchreff <schreffmax@gmail.com>
2024-01-09 11:39:55 +0100

    Error or success message in build script

--- build-arena-unity.sh
+++ build-arena-unity.sh
@@ -4,18 +4,25 @@ unity_location="${HOME}/Unity/Hub/Editor/2022.3.11f1/Editor/Unity"
 
 project_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-build_path="${project_path}/build/arena-unity.x86_64"
+build_path="${project_path}/Build/arena-unity"
 
-build_log="${project_path}/build/build_log.txt"
+build_log="${project_path}/Build/build_log.txt"
 
 ARENA_SIM_SETUP="$(cd "$project_path"/../arena-simulation-setup && pwd)"
 
 # Export arena_simulation_setup location for arena-unity to find files
 export ARENA_SIM_SETUP
 
-echo "Building"
+echo "Building ..."
 
 echo "Please wait for build to finish"
 
 # Build Unity
 "$unity_location" -quit -batchmode -projectpath "$project_path" -buildLinux64Player "$build_path" -logFile "$build_log"
+
+# Check if build was successful
+if [ $? -eq 0 ]; then
+    echo "Build executed successfully."
+else
+    echo "Build failed."
+fi
