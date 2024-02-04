a95071c7ade27997c4443064a7ea625cdb6c7c9f
MaximilianSchreff <schreffmax@gmail.com>
2024-02-04 19:36:09 +0100

    Data class for unity_params.yaml

---
+++ Assets/Scripts/Utils/UnityRobotConfig.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace DataObjects
+{
+    public class RobotUnityConfig
+    {
+        public Dictionary<string, Dictionary<string, object>> components { get; set; }
+    }
+}
