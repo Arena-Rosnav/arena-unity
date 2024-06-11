be407106c5b4aae6f4ef75f78d1ab2045737f15b
Diego <d.diaz@campus.tu-berlin.de>
2024-06-11 15:06:50 +0200

    modified concept script for addressables

--- Assets/Scripts/AssetBundleLoadingTest.cs
+++ Assets/Scripts/AssetBundleLoadingTest.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using System.IO;
 using System.Collections;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.AddressableAssets.ResourceLocators;
 
 // Dev Class for testing the spawning of assets from a single asset bundle.
 // The assets should be built into an asset bundle which is located in Assets/StreamingAssets/AssetBundles/
@@ -24,8 +27,7 @@ public class AddressableSpawn : MonoBehaviour
         "Assets/Resources_moved/Objects/Prefabs/Office/Sofa_1/Sofa_1.prefab",
         "Assets/Resources_moved/Objects/Prefabs/Office/Sofa_2/Sofa_2.prefab"
     };
-
-    private string assetBundleName = "generatedobjects_assets_all_5fcb792c29aa23a8981898b35fb9ea7b.bundle";
+    AsyncOperationHandle<GameObject> opHandle;
 
     // Loading the asset
     IEnumerator Start()
@@ -35,45 +37,33 @@ public class AddressableSpawn : MonoBehaviour
         //Debug.Log(path);
 
         // asynchronous loading of assetbundle 
-        var assetBundleLoadRequest = AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, "AssetBundles", assetBundleName));
-        yield return assetBundleLoadRequest; // return request object while the bundle is loading
+        AsyncOperationHandle<IResourceLocator> handle = Addressables.LoadContentCatalogAsync(Path.Combine(Addressables.RuntimePath, "StandaloneLinux64", "catalog_0.1.json"), true);
+        yield return handle;
 
-        // after loading assign the bundle
-        var assetBundle = assetBundleLoadRequest.assetBundle;
-
-        // if asset bundle loading worked
-        if (assetBundle != null)
+        var i = 0; //enumerate for spawning
+        foreach (var assetName in assetNames)
         {
-            var i = 0; //enumerate for spawning
-            foreach (var assetName in assetNames)
+            // asynchronous loading of asset
+            opHandle = Addressables.LoadAssetAsync<GameObject>(assetName);
+            yield return opHandle;
+
+            // if asset loading worked
+            if (opHandle.Status == AsyncOperationStatus.Succeeded)
             {
-                // asynchronous loading of asset
-                var assetLoadRequest = assetBundle.LoadAssetAsync<GameObject>(assetName);
-                yield return assetLoadRequest;  //return request object while the asset is loading
+                GameObject asset = opHandle.Result;
+                GameObject instance = Instantiate(asset, new Vector3(0, 0, -20 + (3 * i)), Quaternion.identity);
+                i++;
 
-                // after loading assign the asset
-                GameObject asset = assetLoadRequest.asset as GameObject;
-
-                // if asset loading worked
-                if (asset != null)
-                {
-                    GameObject instance = Instantiate(asset, new Vector3(0, 0, -20 + (3 * i)), Quaternion.identity);
-                    i++;
-
-                    // add more actions
-                }
-                else
-                {
-                    Debug.LogError("Asset " + assetName + " could not be found in bundle " + assetBundleName + "!");
-                }
+                // add more actions
             }
-            // unload bundle
-            assetBundle.Unload(false);
-
+            else
+            {
+                Debug.LogError("Asset " + assetName + " could not be found in bundle!");
+            }
         }
-        else
-        {
-            Debug.LogError("Failed to load asset bundle: " + assetBundleName);
-        }
+    }
+    void OnDestroy()
+    {
+        Addressables.Release(opHandle);
     }
 }
